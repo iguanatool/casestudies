4d7443c41f241a7b2a3993a968cb98bf914441eb
Phil McMinn <p.mcminn@sheffield.ac.uk>
2017-01-10 19:14:03 +0000

    Changing names of main directories

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
-ln -s casestudies $IGUANA_HOME/casestudies
-ln -s javasrc $IGUANA_HOME/src/main/java/org/iguanatool/casestudies
+DIR=$( cd ${0%/*} && pwd -P )
+echo $DIR
+#ln -s casestudies $IGUANA_HOME/casestudies
+#ln -s javasrc $IGUANA_HOME/src/main/java/org/iguanatool/casestudies
