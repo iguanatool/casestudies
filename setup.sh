757a45f0c6622747ed433b2982370e5bf0c9bd8d
Phil McMinn <p.mcminn@sheffield.ac.uk>
2017-01-13 09:28:40 +0000

    Modifying setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 if [ -z "$IGUANA_HOME" ]; then
     echo "Error: IGUANA_HOME environement variable not set"
     exit 1
-fi  
+fi
 if [ -z "$IGUANA_CASESTUDIES_HOME" ]; then
     echo "Error: IGUANA_CASESTUDIES_HOME environement variable not set"
     exit 1
-fi  
+fi
 
 cd $IGUANA_HOME
+rm casestudies
 ln -s $IGUANA_CASESTUDIES_HOME/c casestudies
 
 cd $IGUANA_HOME/src/main/java/org/iguanatool/
+rm casestudies
 ln -s $IGUANA_CASESTUDIES_HOME/java casestudies
